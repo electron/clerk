b3bf8b8e46b7a701785f1e0b1d441070be2d6b12
Shelley Vohr <shelley.vohr@gmail.com>
2019-04-27 16:57:49 -0700

    refactor: augment for GitHub Actions (#27)
    This PR migrates Clerk to run as a GitHub Action. It removes Probot and replaces it with actions-toolkit for tidiness, as well as adding some extra logging. All other functionality remains the same.

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM node:10-slim
+
+# Labels for GitHub to read the action
+LABEL "com.github.actions.name"="Clerk"
+LABEL "com.github.actions.description"="Verify PRs have release notes."
+LABEL "com.github.actions.icon"="clipboard"
+LABEL "com.github.actions.color"="gray-dark"
+
+# Copy the package.json and package-lock.json
+COPY package*.json ./
+
+# Install dependencies
+RUN npm ci
+
+# Copy the rest of your action's code
+COPY . .
+
+# Typescript Compilation
+RUN npm run build
+
+# Run `node /entrypoint.js`
+ENTRYPOINT ["node", "/lib/index.js"]
--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,57 @@
-import { Application, Context } from 'probot';
-import { WebhookPayloadWithRepository } from 'probot/lib/context';
+import { Toolkit } from 'actions-toolkit';
 import * as noteUtils from './note-utils';
 
-const submitFeedbackForPR = async (
-  context: Context,
-  pr: WebhookPayloadWithRepository['pull_request'],
-  shouldComment = false,
-) => {
-  const releaseNotes = noteUtils.findNoteInPRBody(context.payload.pull_request.body);
+const submitFeedbackForPR = async (tools: Toolkit, pr: any, shouldComment = false) => {
+  const releaseNotes = noteUtils.findNoteInPRBody(tools.context.payload.pull_request.body);
+  const { owner, repo } = tools.context.repo;
+
   if (!releaseNotes) {
-    await context.github.repos.createStatus(context.repo({
+    await tools.github.repos.createStatus({
+      owner,
+      repo,
       state: 'failure' as 'failure',
       sha: pr.head.sha,
       target_url: 'https://github.com/electron/clerk/blob/master/README.md',
       description: 'Missing release notes',
       context: 'release-notes',
-    }));
+    });
   } else {
-    await context.github.repos.createStatus(context.repo({
+    await tools.github.repos.createStatus({
+      owner,
+      repo,
       state: 'success' as 'success',
       sha: pr.head.sha,
       description: 'Release notes found',
       context: 'release-notes',
-    }));
+    });
   }
 
   if (shouldComment) {
-    await context.github.issues.createComment(context.repo({
+    await tools.github.issues.createComment({
+      owner,
+      repo,
       body: noteUtils.createPRCommentFromNotes(releaseNotes),
       number: pr.number,
-    }));
+    });
   }
 };
 
-const probotRunner = (app: Application) => {
-  app.on('pull_request', async (context) => {
-    // Only respond to events from electron/electron
-    const owner = context.payload.repository.owner.login;
-    const repo = context.payload.repository.name;
-    if (owner !== 'electron' || repo !== 'electron') {
-      console.log(`Not responding to event from: ${owner}/${repo}`);
-      return;
-    }
-
-    const { payload } = context;
+Toolkit.run(
+  async (tools: Toolkit) => {
+    const { payload } = tools.context;
 
     if (payload.action === 'closed' && payload.pull_request.merged) {
-      await submitFeedbackForPR(context, context.payload.pull_request, true);
+      tools.log(`Checking release notes comment on PR #${payload.pull_request.number}`);
+      await submitFeedbackForPR(tools, payload.pull_request, true);
+      tools.exit.success('PR release notes status evaluated.');
     } else if (!payload.pull_request.merged && payload.pull_request.state === 'open') {
       // Only submit feedback for PRs that aren't merged and are open
-      await submitFeedbackForPR(context, context.payload.pull_request);
+      tools.log(`Checking & posting release notes comment on PR #${payload.pull_request.number}`);
+      await submitFeedbackForPR(tools, payload.pull_request);
+      tools.exit.success('PR release notes status evaluated.');
     }
-  });
-};
-
-module.exports = probotRunner;
+  },
+  {
+    event: 'pull_requests',
+  },
+);
