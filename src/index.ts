7c3d60c2337e96761f89b875b026857ea45ff33e
Shelley Vohr <shelley.vohr@gmail.com>
2019-04-27 17:34:40 -0700

    fix: post status on existing check

--- src/index.ts
+++ src/index.ts
@@ -7,35 +7,21 @@ const submitFeedbackForPR = async (tools: Toolkit, pr: any, shouldComment = fals
 
   if (!releaseNotes) {
     tools.log(`No Release Notes: posting failed check.`);
-    await tools.github.repos.createStatus({
-      owner,
-      repo,
-      state: 'failure' as 'failure',
-      sha: pr.head.sha,
-      target_url: 'https://github.com/electron/clerk/blob/master/README.md',
-      description: 'Missing release notes',
-      context: 'release-notes',
-    });
+    tools.exit.failure('Missing release notes.');
   } else {
     tools.log(`Release Notes found: posting successful check.`);
-    await tools.github.repos.createStatus({
-      owner,
-      repo,
-      state: 'success' as 'success',
-      sha: pr.head.sha,
-      description: 'Release notes found',
-      context: 'release-notes',
-    });
-  }
 
-  if (shouldComment) {
-    tools.log(`Creating comment from Release Notes.`);
-    await tools.github.issues.createComment({
-      owner,
-      repo,
-      body: noteUtils.createPRCommentFromNotes(releaseNotes),
-      number: pr.number,
-    });
+    if (shouldComment) {
+      tools.log(`Creating comment from Release Notes.`);
+      await tools.github.issues.createComment({
+        owner,
+        repo,
+        body: noteUtils.createPRCommentFromNotes(releaseNotes),
+        number: pr.number,
+      });
+    }
+
+    tools.exit.success('Release notes found');
   }
 };
 
@@ -46,12 +32,10 @@ Toolkit.run(
     if (payload.action === 'closed' && payload.pull_request.merged) {
       tools.log(`Checking release notes comment on PR #${payload.pull_request.number}`);
       await submitFeedbackForPR(tools, payload.pull_request, true);
-      tools.exit.success('PR release notes status evaluated.');
     } else if (!payload.pull_request.merged && payload.pull_request.state === 'open') {
       // Only submit feedback for PRs that aren't merged and are open
       tools.log(`Checking & posting release notes comment on PR #${payload.pull_request.number}`);
       await submitFeedbackForPR(tools, payload.pull_request);
-      tools.exit.success('PR release notes status evaluated.');
     }
   },
   {
