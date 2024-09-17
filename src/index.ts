7abeaad134c61a1695eb61a6d6dbadfdd0e98503
Samuel Attard <samuel.r.attard@gmail.com>
2024-09-17 12:34:02 -0700

    feat: set no-notes automatically on build PRs

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,34 @@
+import { PullRequest } from '@octokit/webhooks-types/schema';
 import { Probot, Context } from 'probot';
+
 import * as noteUtils from './note-utils';
 
 import d from 'debug';
+import { SEMANTIC_BUILD_PREFIX } from './constants';
 const debug = d('note-utils');
 
 const submitFeedbackForPR = async (
   context: Context<'pull_request'>,
-  pr: any,
+  pr: PullRequest,
   shouldComment = false,
 ) => {
-  const releaseNotes = noteUtils.findNoteInPRBody(pr.body);
+  const releaseNotes = pr.body && noteUtils.findNoteInPRBody(pr.body);
   const github = context.octokit;
 
   if (!releaseNotes) {
     if (pr.user.login === 'dependabot[bot]') {
       debug(`Adding 'Notes: none' to Dependabot PR body`);
+      await github.pulls.update(
+        context.repo({
+          pull_number: pr.number,
+          body: pr.body + '\n\n---\n\nNotes: none',
+        }),
+      );
+      return;
+    }
+
+    if (pr.title.startsWith(SEMANTIC_BUILD_PREFIX)) {
+      debug("Adding 'Notes: none' to build: PR body");
       await github.pulls.update(
         context.repo({
           pull_number: pr.number,
